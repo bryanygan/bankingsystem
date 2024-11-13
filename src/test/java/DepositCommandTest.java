3a04bf4285264d800e4c3fd22743a4a7aa9e39b8
bryangan <byg23@drexel.edu>
2024-11-13 03:29:01 -0500

    Deposit command tests

--- src/test/java/DepositCommandTest.java
+++ src/test/java/DepositCommandTest.java
@@ -1,7 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class DepositCommandTest {
-
 	private Bank bank;
 	private DepositCommand depositCommand;
 
@@ -9,8 +12,47 @@ public class DepositCommandTest {
 	public void setUp() {
 		bank = new Bank();
 		depositCommand = new DepositCommand(bank);
-		Checking checking = new Checking("12345678", 1.0);
-		bank.addAccount(checking);
+		Checking account = new Checking("12345678", 1.0);
+		bank.addAccount(account);
 	}
 
+	@Test
+	public void test_deposit_to_new_account() {
+		String[] command = { "deposit", "12345678", "100" };
+		depositCommand.execute(command);
+		Account account = bank.getAccountByID("12345678");
+
+		assertEquals(100, account.getBalance());
+	}
+
+	@Test
+	public void test_deposit_negative_amount() {
+		String[] command = { "deposit", "12345678", "-50" };
+		Account account = bank.getAccountByID("12345678");
+		double initialBalance = account.getBalance();
+		depositCommand.execute(command);
+
+		assertEquals(account.getBalance(), initialBalance);
+	}
+
+	@Test
+	public void test_deposit_to_non_existent_account() {
+		String accountId = "99999999";
+		String[] command = { "deposit", accountId, "50" };
+		assertNull(bank.getAccountByID(accountId));
+		depositCommand.execute(command);
+
+		assertNull(bank.getAccountByID(accountId), "Account should still not exist after deposit attempt.");
+	}
+
+	@Test
+	public void test_deposit_to_existing_account() {
+		String[] command1 = { "deposit", "12345678", "50" };
+		String[] command2 = { "deposit", "12345678", "100" };
+		depositCommand.execute(command1);
+		depositCommand.execute(command2);
+		Account account = bank.getAccountByID("12345678");
+
+		assertEquals(150, account.getBalance());
+	}
 }
