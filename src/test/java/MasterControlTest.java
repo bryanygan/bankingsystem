0553926d32462e5f7ef3b5ee6c76942cbda948ab
bryangan <byg23@drexel.edu>
2024-11-13 21:19:05 -0500

    List for MasterControl, test for empty list

--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -1,10 +1,30 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MasterControlTest {
+	private Bank bank;
+	private CommandProcessor commandProcessor;
+	private InvalidCommands invalidCommands;
+	private MasterControl masterControl;
 
-	MasterControl masterControl;
+	@BeforeEach
+	public void setUp() {
+		bank = new Bank();
+		commandProcessor = new CommandProcessor(bank);
+		invalidCommands = new InvalidCommands();
+		masterControl = new MasterControl(bank, commandProcessor, invalidCommands);
+	}
 
-	void setUp() {
-		Bank bank = new Bank();
-		masterControl = new MasterControl(new CommandValidaton(bank), new CommandProcessor(bank),
-				new InvalidCommands());
+	@Test
+	public void test_empty_command_list() {
+		List<String> commands = new ArrayList<>();
+		List<String> result = masterControl.start(commands);
+
+		assertEquals(0, result.size());
 	}
 }
