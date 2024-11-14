581801c0e2db12777cf4c9a12d5092521ab5b05c
bryangan <byg23@drexel.edu>
2024-11-13 21:28:25 -0500

    Valid commands process

--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -27,4 +27,14 @@ public class MasterControlTest {
 
 		assertEquals(0, result.size());
 	}
+
+	@Test
+	public void test_process_valid_command() {
+		List<String> commands = new ArrayList<>();
+		commands.add("create checking 12345678 1.0"); // valid command
+
+		List<String> result = masterControl.start(commands);
+
+		assertEquals(0, result.size());
+	}
 }
