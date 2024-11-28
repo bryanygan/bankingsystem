f3521d8fe5b0a529dae571f2a5c0a02976855eee
bryangan <byg23@drexel.edu>
2024-11-28 14:39:51 -0500

    withdraw more than balance test

--- src/test/java/WithdrawCommandProcessorTest.java
+++ src/test/java/WithdrawCommandProcessorTest.java
@@ -17,4 +17,19 @@ public class WithdrawCommandProcessorTest {
 
 		assertEquals(200, Bank.getAccountByID("12345678").getBalance());
 	}
+
+	@Test
+	public void test_withdraw_more_than_balance() {
+		Bank bank = new Bank();
+		Checking checking = new Checking("12345678", 3.5);
+		checking.deposit(300);
+		Bank.addAccount(checking);
+		WithdrawCommandProcessor processor = new WithdrawCommandProcessor();
+
+		String command = "withdraw 12345678 400";
+		processor.processCommand(command, bank);
+
+		assertEquals(0, Bank.getAccountByID("12345678").getBalance());
+	}
+
 }
