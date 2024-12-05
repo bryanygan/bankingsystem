f087dab7e0fdf84bfbbe0070d0d2bee1061feb5f
bryangan <byg23@drexel.edu>
2024-12-05 15:50:40 -0500

    validate transfer command parts

--- src/main/java/TransferCommand.java
+++ src/main/java/TransferCommand.java
@@ -9,7 +9,32 @@ public class TransferCommand {
 	}
 
 	public boolean validate(Bank bank) {
-		return false;
+		String[] parts = rawCommand.split(" ");
+		if (parts.length != 4)
+			return false;
+
+		fromId = parts[1];
+		toId = parts[2];
+
+		try {
+			amount = Double.parseDouble(parts[3]);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		if (amount <= 0 || fromId.equals(toId))
+			return false;
+
+		Account fromAccount = Bank.getAccountByID(fromId);
+		Account toAccount = Bank.getAccountByID(toId);
+
+		if (fromAccount == null || toAccount == null)
+			return false;
+
+		if (fromAccount.getType().equals("CD") || toAccount.getType().equals("CD"))
+			return false;
+
+		return true;
 	}
 
 	public void execute(Bank bank) {
---
+++ src/test/java/TransferCommandTest.java
@@ -0,0 +1,20 @@
+public class TransferCommandTest {
+
+	/*
+	 * @Test public void test_valid_transfer_between_checking_accounts() { Bank bank
+	 * = new Bank(); Checking checking = new Checking("12345678", 0.03);
+	 * Bank.addAccount(checking); Checking checking2 = new Checking("87654321",
+	 * 0.02); Bank.addAccount(checking); Bank.addAccount(checking2);
+	 * 
+	 * checking.deposit(500); checking2.deposit(300);
+	 * 
+	 * TransferCommand transfer = new
+	 * TransferCommand("transfer 12345678 87654321 200"); boolean isValid =
+	 * transfer.validate(bank);
+	 * 
+	 * assertTrue(isValid); transfer.execute(bank);
+	 * 
+	 * assertEquals(300, Bank.getAccountByID("12345678").getBalance());
+	 * assertEquals(500, Bank.getAccountByID("87654321").getBalance()); }
+	 */
+}
