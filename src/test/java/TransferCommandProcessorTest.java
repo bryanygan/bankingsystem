8250310baba7fc60ddfcdea3c74faec7ea9a9f02
bryangan <byg23@drexel.edu>
2024-12-06 02:48:06 -0500

    tests for valid transfers and edge cases

--- src/test/java/TransferCommandProcessorTest.java
+++ src/test/java/TransferCommandProcessorTest.java
@@ -70,4 +70,120 @@ public class TransferCommandProcessorTest {
 		assertThrows(IllegalArgumentException.class, () -> processor.process(command, bank));
 	}
 
+	@Test
+	public void test_transfer_between_savings_accounts() {
+		Bank bank = new Bank();
+
+		Savings fromAccount = new Savings("12345678", 0.05);
+		fromAccount.deposit(1500);
+		Bank.addAccount(fromAccount);
+
+		Savings toAccount = new Savings("87654321", 0.05);
+		toAccount.deposit(500);
+		Bank.addAccount(toAccount);
+
+		TransferCommand command = new TransferCommand("12345678", "87654321", 1000, "transfer 12345678 87654321 1000");
+		TransferCommandProcessor processor = new TransferCommandProcessor();
+		processor.process(command, bank);
+
+		assertEquals(500.0, fromAccount.getBalance(), 0.01);
+		assertEquals(1500.0, toAccount.getBalance(), 0.01);
+	}
+
+	@Test
+	public void test_valid_transfer_from_checking_to_savings() {
+		Bank bank = new Bank();
+
+		Checking checking = new Checking("12345678", 0.03);
+		checking.deposit(500);
+		Bank.addAccount(checking);
+
+		Savings savings = new Savings("87654321", 0.05);
+		savings.deposit(300);
+		Bank.addAccount(savings);
+
+		TransferCommand command = new TransferCommand("12345678", "87654321", 200, "transfer 12345678 87654321 200");
+		TransferCommandProcessor processor = new TransferCommandProcessor();
+		processor.process(command, bank);
+
+		assertEquals(300.0, checking.getBalance(), 0.01); // Remaining balance
+		assertEquals(500.0, savings.getBalance(), 0.01); // Updated balance
+	}
+
+	@Test
+	public void test_valid_transfer_from_savings_to_checking() {
+		Bank bank = new Bank();
+
+		Savings savings = new Savings("12345678", 0.05);
+		savings.deposit(1000);
+		Bank.addAccount(savings);
+
+		Checking checking = new Checking("87654321", 0.03);
+		checking.deposit(300);
+		Bank.addAccount(checking);
+
+		TransferCommand command = new TransferCommand("12345678", "87654321", 700, "transfer 12345678 87654321 700");
+		TransferCommandProcessor processor = new TransferCommandProcessor();
+		processor.process(command, bank);
+
+		assertEquals(300.0, savings.getBalance(), 0.01); // Remaining balance
+		assertEquals(1000.0, checking.getBalance(), 0.01); // Updated balance
+	}
+
+	@Test
+	public void test_partial_transfer_due_to_insufficient_funds_in_checking() {
+		Bank bank = new Bank();
+
+		Checking checking = new Checking("12345678", 0.03);
+		checking.deposit(300);
+		Bank.addAccount(checking);
+
+		Savings savings = new Savings("87654321", 0.05);
+		savings.deposit(500);
+		Bank.addAccount(savings);
+
+		TransferCommand command = new TransferCommand("12345678", "87654321", 500, "transfer 12345678 87654321 500");
+		TransferCommandProcessor processor = new TransferCommandProcessor();
+		processor.process(command, bank);
+
+		assertEquals(0.0, checking.getBalance(), 0.01); // All funds withdrawn
+		assertEquals(800.0, savings.getBalance(), 0.01); // Updated balance
+	}
+
+	@Test
+	public void test_transfer_with_zero_amount() {
+		Bank bank = new Bank();
+
+		Checking checking = new Checking("12345678", 0.03);
+		checking.deposit(500);
+		Bank.addAccount(checking);
+
+		Savings savings = new Savings("87654321", 0.05);
+		savings.deposit(300);
+		Bank.addAccount(savings);
+
+		TransferCommand command = new TransferCommand("12345678", "87654321", 0, "transfer 12345678 87654321 0");
+		TransferCommandProcessor processor = new TransferCommandProcessor();
+
+		assertThrows(IllegalArgumentException.class, () -> processor.process(command, bank));
+	}
+
+	@Test
+	public void test_transfer_with_negative_amount() {
+		Bank bank = new Bank();
+
+		Checking checking = new Checking("12345678", 0.03);
+		checking.deposit(500);
+		Bank.addAccount(checking);
+
+		Savings savings = new Savings("87654321", 0.05);
+		savings.deposit(300);
+		Bank.addAccount(savings);
+
+		TransferCommand command = new TransferCommand("12345678", "87654321", -200, "transfer 12345678 87654321 -200");
+		TransferCommandProcessor processor = new TransferCommandProcessor();
+
+		assertThrows(IllegalArgumentException.class, () -> processor.process(command, bank));
+	}
+
 }
