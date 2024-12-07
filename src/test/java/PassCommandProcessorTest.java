0fcca7b77f9a3718b991f93519840ffb1170ae42
bryangan <byg23@drexel.edu>
2024-12-06 19:27:32 -0500

    invalid without pass word in command

--- src/test/java/PassCommandProcessorTest.java
+++ src/test/java/PassCommandProcessorTest.java
@@ -1,2 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PassCommandProcessorTest {
+
+	private PassCommandValidator validator;
+
+	@BeforeEach
+	public void setUp() {
+		validator = new PassCommandValidator();
+	}
+
+	@Test
+	public void test_invalid_command_without_pass_word() {
+		assertFalse(validator.validate("hello"));
+		assertFalse(validator.validate(" "));
+		assertFalse(validator.validate("passing"));
+	}
 }
