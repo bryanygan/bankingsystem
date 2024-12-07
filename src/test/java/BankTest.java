dd09e1cdd0317a8fdffc38aa72f3277b7fc4949a
bryangan <byg23@drexel.edu>
2024-12-06 20:45:39 -0500

    bank being accents via instance reference warning

--- src/test/java/BankTest.java
+++ src/test/java/BankTest.java
@@ -8,24 +8,24 @@ public class BankTest {
 	@Test
 	public void no_accounts_when_bank_is_created() {
 		Bank bank = new Bank();
-		assertNull(bank.getAccountByID("12345678"));
+		assertNull(Bank.getAccountByID("12345678"));
 	}
 
 	@Test
 	public void bank_adds_account() {
 		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 3.5);
-		bank.addAccount(checking);
-		assertEquals("12345678", bank.getAccountByID("12345678").getAccountID());
+		Bank.addAccount(checking);
+		assertEquals("12345678", Bank.getAccountByID("12345678").getAccountID());
 	}
 
 	@Test
 	public void bank_adds_two_accounts() {
 		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 3.5);
-		bank.addAccount(checking);
+		Bank.addAccount(checking);
 		Savings savings = new Savings("23456781", 1.5);
-		bank.addAccount(savings);
+		Bank.addAccount(savings);
 		assertEquals(2, bank.getNumberOfAccounts());
 	}
 
@@ -34,9 +34,9 @@ public class BankTest {
 		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 3.5);
 		Savings savings = new Savings("23456781", 1.5);
-		bank.addAccount(checking);
-		bank.addAccount(savings);
-		Account retrievedAccount = bank.getAccountByID("12345678");
+		Bank.addAccount(checking);
+		Bank.addAccount(savings);
+		Account retrievedAccount = Bank.getAccountByID("12345678");
 		assertEquals(checking, retrievedAccount);
 		assertEquals("12345678", retrievedAccount.getAccountID());
 	}
@@ -45,7 +45,7 @@ public class BankTest {
 	public void bank_deposits_to_correct_account() {
 		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 3.5);
-		bank.addAccount(checking);
+		Bank.addAccount(checking);
 		bank.depositByID("12345678", 500);
 		assertEquals(500, checking.getBalance());
 	}
@@ -54,7 +54,7 @@ public class BankTest {
 	public void bank_withdraws_from_correct_account() {
 		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 3.5);
-		bank.addAccount(checking);
+		Bank.addAccount(checking);
 		bank.depositByID("12345678", 500);
 		bank.withdrawByID("12345678", 200);
 		assertEquals(300, checking.getBalance());
@@ -64,7 +64,7 @@ public class BankTest {
 	public void two_deposits_through_bank() {
 		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 3.5);
-		bank.addAccount(checking);
+		Bank.addAccount(checking);
 		bank.depositByID("12345678", 500);
 		bank.depositByID("12345678", 200);
 		assertEquals(700, checking.getBalance());
@@ -74,7 +74,7 @@ public class BankTest {
 	public void two_withdraws_through_bank() {
 		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 3.5);
-		bank.addAccount(checking);
+		Bank.addAccount(checking);
 		bank.depositByID("12345678", 500);
 		bank.withdrawByID("12345678", 200);
 		bank.withdrawByID("12345678", 200);
