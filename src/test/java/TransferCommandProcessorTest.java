c5a9b7d94d0558088c431a0b06c7160ec2fa2261
bryangan <byg23@drexel.edu>
2024-12-06 19:30:32 -0500

    refactoring to simplify

--- src/test/java/TransferCommandProcessorTest.java
+++ src/test/java/TransferCommandProcessorTest.java
@@ -1,13 +1,22 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TransferCommandProcessorTest {
 
+	private Bank bank;
+	private TransferCommandProcessor processor;
+
+	@BeforeEach
+	public void setUp() {
+		bank = new Bank();
+		processor = new TransferCommandProcessor();
+	}
+
 	@Test
 	public void test_process_valid_transfer() {
-		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 0.03);
 		checking.deposit(500);
 		Bank.addAccount(checking);
@@ -16,7 +25,6 @@ public class TransferCommandProcessorTest {
 		Bank.addAccount(savings);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", 300, "transfer 12345678 87654321 300");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 		processor.process(command, bank);
 
 		assertEquals(200.0, Bank.getAccountByID("12345678").getBalance(), 0.01); // Remaining balance
@@ -25,7 +33,6 @@ public class TransferCommandProcessorTest {
 
 	@Test
 	public void test_process_partial_transfer_due_to_insufficient_balance() {
-		Bank bank = new Bank();
 		Checking checking = new Checking("12345678", 0.03);
 		Bank.addAccount(checking);
 
@@ -35,7 +42,6 @@ public class TransferCommandProcessorTest {
 		checking.deposit(200);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", 300, "transfer 12345678 87654321 300");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 		processor.process(command, bank);
 
 		assertEquals(0.0, Bank.getAccountByID("12345678").getBalance(), 0.01); // All withdrawn
@@ -44,36 +50,28 @@ public class TransferCommandProcessorTest {
 
 	@Test
 	public void test_invalid_transfer_to_nonexistent_account() {
-		Bank bank = new Bank();
-
 		Checking fromAccount = new Checking("12345678", 0.03);
 		fromAccount.deposit(500);
 		Bank.addAccount(fromAccount);
 
 		TransferCommand command = new TransferCommand("12345678", "99999999", 200, "transfer 12345678 99999999 200");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 
 		assertThrows(IllegalArgumentException.class, () -> processor.process(command, bank));
 	}
 
 	@Test
 	public void test_invalid_transfer_from_nonexistent_account() {
-		Bank bank = new Bank();
-
 		Checking toAccount = new Checking("87654321", 0.02);
 		toAccount.deposit(300);
 		Bank.addAccount(toAccount);
 
 		TransferCommand command = new TransferCommand("99999999", "87654321", 200, "transfer 99999999 87654321 200");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 
 		assertThrows(IllegalArgumentException.class, () -> processor.process(command, bank));
 	}
 
 	@Test
 	public void test_transfer_between_savings_accounts() {
-		Bank bank = new Bank();
-
 		Savings fromAccount = new Savings("12345678", 0.05);
 		fromAccount.deposit(1500);
 		Bank.addAccount(fromAccount);
@@ -83,7 +81,6 @@ public class TransferCommandProcessorTest {
 		Bank.addAccount(toAccount);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", 1000, "transfer 12345678 87654321 1000");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 		processor.process(command, bank);
 
 		assertEquals(500.0, fromAccount.getBalance(), 0.01);
@@ -92,8 +89,6 @@ public class TransferCommandProcessorTest {
 
 	@Test
 	public void test_valid_transfer_from_checking_to_savings() {
-		Bank bank = new Bank();
-
 		Checking checking = new Checking("12345678", 0.03);
 		checking.deposit(500);
 		Bank.addAccount(checking);
@@ -103,7 +98,6 @@ public class TransferCommandProcessorTest {
 		Bank.addAccount(savings);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", 200, "transfer 12345678 87654321 200");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 		processor.process(command, bank);
 
 		assertEquals(300.0, checking.getBalance(), 0.01); // Remaining balance
@@ -112,8 +106,6 @@ public class TransferCommandProcessorTest {
 
 	@Test
 	public void test_valid_transfer_from_savings_to_checking() {
-		Bank bank = new Bank();
-
 		Savings savings = new Savings("12345678", 0.05);
 		savings.deposit(1000);
 		Bank.addAccount(savings);
@@ -123,7 +115,6 @@ public class TransferCommandProcessorTest {
 		Bank.addAccount(checking);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", 700, "transfer 12345678 87654321 700");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 		processor.process(command, bank);
 
 		assertEquals(300.0, savings.getBalance(), 0.01); // Remaining balance
@@ -132,8 +123,6 @@ public class TransferCommandProcessorTest {
 
 	@Test
 	public void test_partial_transfer_due_to_insufficient_funds_in_checking() {
-		Bank bank = new Bank();
-
 		Checking checking = new Checking("12345678", 0.03);
 		checking.deposit(300);
 		Bank.addAccount(checking);
@@ -143,7 +132,6 @@ public class TransferCommandProcessorTest {
 		Bank.addAccount(savings);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", 500, "transfer 12345678 87654321 500");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 		processor.process(command, bank);
 
 		assertEquals(0.0, checking.getBalance(), 0.01); // All funds withdrawn
@@ -152,8 +140,6 @@ public class TransferCommandProcessorTest {
 
 	@Test
 	public void test_transfer_with_zero_amount() {
-		Bank bank = new Bank();
-
 		Checking checking = new Checking("12345678", 0.03);
 		checking.deposit(500);
 		Bank.addAccount(checking);
@@ -163,15 +149,12 @@ public class TransferCommandProcessorTest {
 		Bank.addAccount(savings);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", 0, "transfer 12345678 87654321 0");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 
 		assertThrows(IllegalArgumentException.class, () -> processor.process(command, bank));
 	}
 
 	@Test
 	public void test_transfer_with_negative_amount() {
-		Bank bank = new Bank();
-
 		Checking checking = new Checking("12345678", 0.03);
 		checking.deposit(500);
 		Bank.addAccount(checking);
@@ -181,9 +164,7 @@ public class TransferCommandProcessorTest {
 		Bank.addAccount(savings);
 
 		TransferCommand command = new TransferCommand("12345678", "87654321", -200, "transfer 12345678 87654321 -200");
-		TransferCommandProcessor processor = new TransferCommandProcessor();
 
 		assertThrows(IllegalArgumentException.class, () -> processor.process(command, bank));
 	}
-
 }
